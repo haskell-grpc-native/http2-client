d47d3d2afd9bf5bce52ca7dc1f3a905da9b8a338
Lucas DiCioccio <lucas@dicioccio.fr>
2017-08-13 20:47:23 +0000

    Add script to run the test client against hosts.

---
+++ scripts/collect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+query() {
+  local verb=$1
+  local host=$2
+  local port=$3
+  local path=$4
+
+  http2-client-exe \
+	--initial-window-kick 10000000 \
+	--verb $verb \
+	--host $host \
+	--port $port \
+	--path $path
+}
+
+commit=`git rev-parse HEAD | head -c 8`
+
+mkdir -p $commit
+
+for host in `grep -v '^#' $1`; do
+	path="${commit}/${host}"
+	echo "**************************************"
+	echo $host
+        query GET $host 443 / > $path
+done
