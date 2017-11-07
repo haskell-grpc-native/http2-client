02d8ed523ab74ddf8ea0bab22fa8d888d0797888
Lucas DiCioccio <lucas@dicioccio.fr>
2017-11-07 22:28:36 +0000

    Move per-stream client frame duping before initializeStream.

--- src/Network/HTTP2/Client.hs
+++ src/Network/HTTP2/Client.hs
@@ -396,9 +396,10 @@ runHttp2Client conn encoderBufSize decoderBufSize initSettings goAwayHandler fal
                     else do
                         cont <- withClientStreamId $ \sid -> do
                             streamHeaders <- dupChan serverHeaders
+                            streamFrames <- dupChan serverFrames
                             initializeStream conn
                                              settings
-                                             serverFrames
+                                             streamFrames
                                              streamHeaders
                                              serverPushPromises
                                              hpackEncoder
@@ -446,12 +447,11 @@ initializeStream
   -> StreamId
   -> (Http2Stream -> StreamDefinition a)
   -> IO (IO a)
-initializeStream conn settings serverFrames headersFrames serverPushPromises hpackEncoder sid getWork = do
+initializeStream conn settings frames headersFrames serverPushPromises hpackEncoder sid getWork = do
     let frameStream = makeFrameClientStream conn sid
 
     -- Register interest in frames.
-    frames  <- dupChan serverFrames
-    credits <- dupChan serverFrames
+    credits <- dupChan frames
     pushPromises <- dupChan serverPushPromises
 
     -- Builds a flow-control context.
