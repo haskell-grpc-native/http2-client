947c494d2d4bb9214de8d55b182f5a644737cdfd
Lucas DiCioccio <lucas@dicioccio.fr>
2017-11-15 22:51:10 +0000

    Instantiate an HTTP1.1 / connexion.

--- src/Network/HTTP2/Client/RawConnection.hs
+++ src/Network/HTTP2/Client/RawConnection.hs
@@ -8,7 +8,11 @@ module Network.HTTP2.Client.RawConnection (
     ) where
 
 import           Data.ByteString (ByteString)
-import           Network.Connection (connectTo, initConnectionContext, ConnectionParams(..), TLSSettings(..), connectionPut, connectionGetExact, connectionClose)
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 as Char8
+import qualified Data.ByteString.Base64 as Base64
+import           Data.Monoid ((<>))
+import           Network.Connection (Connection, connectionGetLine, connectTo, initConnectionContext, ConnectionParams(..), TLSSettings(..), connectionPut, connectionGetExact, connectionClose)
 import qualified Network.HTTP2 as HTTP2
 import           Network.Socket (HostName, PortNumber)
 import qualified Network.TLS as TLS
@@ -35,19 +39,65 @@ newRawHttp2Connection :: HostName
                       -- overwritten to always return ["h2", "h2-17"].
                       -> IO RawHttp2Connection
 newRawHttp2Connection host port mparams = do
-    -- Connects to SSL.
-    ctx <- initConnectionContext
-    conn <- connectTo ctx connParams
+    conn <- newRawStream host port mparams
+    -- Sends the HTTP2 preface.
+    connectionPut conn HTTP2.connectionPreface
+    return $ wrapConnection conn
 
-    -- Define raw byte-stream handlers.
+newRawH2cConnection :: HostName
+                    -- ^ Server's hostname.
+                    -> PortNumber
+                    -- ^ Server's port to connect to.
+                    -> HTTP2.SettingsList
+                    -- ^ Initial SETTINGS for the first query.
+                    -> IO RawHttp2Connection
+newRawH2cConnection host port settings = do
+    -- TODO: allow hijacking HTTP query (including: path, verb, headers)
+    conn <- newRawStream host port Nothing
+    -- Initializes the HTTP1.1 upgrade.
+    connectionPut conn $ upgradeFromHTTP1req host settings
+    let loop = do
+            x <- connectionGetLine 8096 conn
+            if ByteString.null x then return () else loop
+    -- Sends the HTTP2 preface.
+    connectionPut conn HTTP2.connectionPreface
+    return $ wrapConnection conn
+
+upgradeFromHTTP1req :: HostName -> HTTP2.SettingsList -> ByteString
+upgradeFromHTTP1req host setts = Char8.unlines [
+    "GET / HTTP/1.1"
+  , "Host: " <> Char8.pack host
+  , "Connection: Upgrade, HTTP2-Settings"
+  , "Upgrade: h2c"
+  , "HTTP2-Settings: " <> Base64.encode encodedSettings
+  , ""
+  ]
+  where
+    encodedSettings = HTTP2.encodeFrame (HTTP2.encodeInfo id 1) (HTTP2.SettingsFrame setts)
+
+wrapConnection :: Connection -> RawHttp2Connection
+wrapConnection conn =
     let putRaw dat    = connectionPut conn dat
-    let getRaw amount = connectionGetExact conn amount
-    let doClose       = connectionClose conn
+        getRaw amount = connectionGetExact conn amount
+        doClose       = connectionClose conn
 
-    -- Initializes the HTTP2 stream.
-    putRaw HTTP2.connectionPreface
+    in RawHttp2Connection putRaw getRaw doClose
 
-    return $ RawHttp2Connection putRaw getRaw doClose
+-- | Initiates a RawHttp2Connection with a server.
+--
+-- The current code does not handle closing the connexion, yikes.
+newRawStream :: HostName
+            -- ^ Server's hostname.
+            -> PortNumber
+            -- ^ Server's port to connect to.
+            -> Maybe TLS.ClientParams
+            -- ^ TLS parameters. The 'TLS.onSuggestALPN' hook is
+            -- overwritten to always return ["h2", "h2-17"].
+            -> IO Connection
+newRawStream host port mparams = do
+    -- Connects to SSL or TCP. Over TLS we'll need to negotiate "h2" in ALPN.
+    ctx <- initConnectionContext
+    connectTo ctx connParams
   where
     overwriteALPNHook params = (TLS.clientHooks params) {
         TLS.onSuggestALPN = return $ Just [ "h2", "h2-17" ]
